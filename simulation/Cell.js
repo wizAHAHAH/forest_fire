90febbb99a9ab79aa000618e58aaf22a40e7e604
yhwach <2danilaproduction@gmail.com>
2026-06-29 15:27:44 +0700

    Creating a class Cell

---
+++ simulation/Cell.js
@@ -0,0 +1,49 @@
+// типы местности
+export const TERRAIN = {
+    NORMAL: 'NORMAL',
+    WET: 'WET',
+    DRY: 'DRY',
+};
+
+// вероятность возгорания для каждой местности
+const TERRAIN_MULTIPLIER = {
+    [TERRAIN.NORMAL]: 1.0,
+    [TERRAIN.WET]: 0.4,
+    [TERRAIN.DRY]: 1.6,
+};
+
+// состояние клетки
+export const STATE = {
+    EMPTY: 'EMPTY',
+    TREE: 'TREE',
+    FIRE: 'FIRE',
+    BURNED: 'BURNED',
+};
+
+export class Cell {
+    constructor(x, y, terrain = TERRAIN.NORMAL){
+        this.x = x;
+        this.y = y;
+        this.terrain = terrain;
+        this.state = STATE.EMPTY;
+    }
+
+
+// влияние местности на вероятность возгорания
+
+    terrainMultiplier(){
+        return TERRAIN_MULTIPLIER[this.terrain];
+    }
+
+    isTree() {
+        return this.state === STATE.TREE;
+    }
+
+    isFire() {
+        return this.state === STATE.FIRE;
+    }
+
+    isBurned() {
+        return this.state === STATE.BURNED;
+    }
+}
